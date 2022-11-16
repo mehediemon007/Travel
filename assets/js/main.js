a8ee8a2c08cee95662a415a38b442064c5816928
Akaarit-Frontend <mehedi.emon@akaarit.com>
2022-11-16 10:45:54 +0600

    first comit

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+;(function($){
+
+    // WOW Animation
+    
+    $(document).ready(function () {
+        new WOW().init();
+    });
+
+    // Swipe Slider
+
+    // $(document).ready(function () {
+    //     var swiper = new Swiper(".hero-banner-wpr", {
+    //       autoplay: {
+    //         delay: 5000,
+    //         disableOnInteraction: false
+    //       },
+    //       speed: 500,
+    //       loop: true,
+    //       pagination: {
+    //         el: ".swiper-pagination",
+    //         type: "fraction"
+    //       },
+    //       navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev"
+    //       },
+    //       on: {
+    //         init: function () {
+    //           $(".swiper-progress-bar").removeClass("animate");
+    //           $(".swiper-progress-bar").removeClass("active");
+    //           $(".swiper-progress-bar").eq(0).addClass("animate");
+    //           $(".swiper-progress-bar").eq(0).addClass("active");
+    //         },
+    //         slideChangeTransitionStart: function () {
+    //           $(".swiper-progress-bar").removeClass("animate");
+    //           $(".swiper-progress-bar").removeClass("active");
+    //           $(".swiper-progress-bar").eq(0).addClass("active");
+    //         },
+    //         slideChangeTransitionEnd: function () {
+    //           $(".swiper-progress-bar").eq(0).addClass("animate");
+    //         }
+    //       }
+    //     });
+    //     $(".swiper-container").hover(function () {
+    //       swiper.autoplay.stop();
+    //       $(".swiper-progress-bar").removeClass("animate");
+    //     }, function () {
+    //       swiper.autoplay.start();
+    //       $(".swiper-progress-bar").addClass("animate");
+    //     });
+    // });
+
+    var swiper = new Swiper(".trv-hero-banner", {
+        // navigation: {
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        // },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        speed: 500,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction"
+        },
+        on: {
+            init: function () {
+              $(".swiper-progress-bar").removeClass("animate");
+              $(".swiper-progress-bar").removeClass("active");
+              $(".swiper-progress-bar").eq(0).addClass("animate");
+              $(".swiper-progress-bar").eq(0).addClass("active");
+            },
+            slideChangeTransitionStart: function () {
+              $(".swiper-progress-bar").removeClass("animate");
+              $(".swiper-progress-bar").removeClass("active");
+              $(".swiper-progress-bar").eq(0).addClass("active");
+            },
+            slideChangeTransitionEnd: function () {
+              $(".swiper-progress-bar").eq(0).addClass("animate");
+            }
+        }
+    });
+      
+
+})(jQuery);
+
+(function(){
+
+    // Sticky Nav
+
+    const headerContainer = document.querySelector(".clo-header.fixed");
+
+    if(headerContainer != null && window.screen.width > 991){
+        window.onscroll = ()=>{
+            this.scrollY > 100 ? headerContainer.style.display = 'none' : headerContainer.style.display = 'block';
+        }
+    }
+
+})()
